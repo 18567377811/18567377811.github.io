aece7b4956064aa4d32abe18113d0c702f184861
deng_ziyang <deng_ziyang@foxmail.com>
2022-12-05 16:37:27 +0800

    邓子洋路由axioss基本封装

--- RongCat_App/src/router/routes.js
+++ RongCat_App/src/router/routes.js
@@ -1,1 +1,17 @@
 // 配置路由
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('../components/Home.vue'),
+      children: [
+        {}
+      ]
+    }
+  ]
+})
+
+export default router;
