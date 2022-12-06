f67d064855c10bb4433a25baf97d70b95e2e237d
deng_ziyang <deng_ziyang@foxmail.com>
2022-12-06 21:52:13 +0800

    底部导航栏路由配置

--- RongCat_App/src/App.vue
+++ RongCat_App/src/App.vue
@@ -1,11 +1,40 @@
 <template>
   <div class="App">
     <router-view></router-view>
+    <van-tabbar v-model="active">
+      <van-tabbar-item to="/">
+        <template #icon="props">
+          <img src="./images/Slice15.png" />
+        </template>首页
+      </van-tabbar-item>
+      <van-tabbar-item to="/select">
+        <template #icon="props">
+          <img src="./images/Slice 16.png" />
+        </template>严选
+      </van-tabbar-item>
+      <van-tabbar-item to="/">
+        <template #icon="props">
+          <img src="./images/Slice 17.png" />
+        </template>榜单
+      </van-tabbar-item>
+      <van-tabbar-item to="/">
+        <template #icon="props">
+          <img src="./images/Slice 18.png" />
+        </template>圈子
+      </van-tabbar-item>
+      <van-tabbar-item to="/My">
+        <template #icon="props">
+          <img src="./images/Slice 19.png" />
+        </template>我的
+      </van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const active = ref(0);
 </script>
 
 <style lang="scss" scoped>
--- RongCat_App/src/main.js
+++ RongCat_App/src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-
 import router from "./router/routes";
 import Vant from "vant";
 import "vant/lib/index.css";
+import 'lib-flexible';
 
 createApp(App).use(Vant).use(router).mount("#app");
