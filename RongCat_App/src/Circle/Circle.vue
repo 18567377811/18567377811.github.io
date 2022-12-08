1879d023ac862433c0e86cdd4453ee782fb3d9bd
NaiTe <984197929@qq.com>
2022-12-08 21:19:54 +0800

    圈子首页架构

--- RongCat_App/src/Circle/Circle.vue
+++ RongCat_App/src/Circle/Circle.vue
@@ -1,5 +1,49 @@
-<style scoped></style>
 <template>
-  <div>circle</div>
+  <div>
+    circle
+    <!-- 导航栏 -->
+    <div class="Circle_Nav">
+      <div class="Circle_Nav_title">
+        <li>达人</li>
+        <li>推荐</li>
+        <li>圈子</li>
+      </div>
+      <div class="Circle_Nav_plus">
+        <img src="./images/ico/Slice 132.png" alt="" />
+      </div>
+    </div>
+    <!-- 圈子 -->
+    <div class="Circle_HotCircle">
+      <li>热门圈子</li>
+      <li>查看全部></li>
+      <ul>
+        <li>
+          <img src="./images/Slice 67.png" alt="" />
+        </li>
+      </ul>
+    </div>
+    <!-- 达人推荐 -->
+    <div class="Circle_Daren">
+      <li>达人推荐</li>
+      <li>换一换</li>
+      <ul>
+        <li>
+          <img src="./images/Slice 72.png" alt="" />
+        </li>
+      </ul>
+    </div>
+    <!-- 广场 -->
+    <div class="Circle_Plaza">
+      <li>广场</li>
+      <li>更多></li>
+      <ul>
+        <li>
+          <img src="./images/Slice 9.png" alt="" />
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import "./Circle.css";
+</script>
