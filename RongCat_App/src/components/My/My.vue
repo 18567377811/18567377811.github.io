3db3dee56b2deaf6916ed42e3ce90e60b7036468
xiaoye <1742414912@qq.com>
2022-12-11 23:25:45 +0800

    我的布局完成

--- RongCat_App/src/components/My/My.vue
+++ RongCat_App/src/components/My/My.vue
@@ -24,11 +24,91 @@
       </div>
     </div>
     <div class="you">
-      <span>></span>
+      <span>></span></div>
+    
+    <div class="pin">
+      <div class="pinglun">
+      <div class="top">
+        <grid>
+          <grid-item icon="photo-o" text="日记" />
+          <grid-item icon="photo-o" text="评论" />
+          <grid-item icon="photo-o" text="收藏" />
+          <grid-item icon="photo-o" text="喜欢" />
+        </grid>
+      </div>
+      <div class="bottom">
+        <img src="./images/Slice 135.jpg" alt="">
+        <span class="sp-a">0金币</span>
+        <span class="sp-b">邀请好友最高可得888金币~</span>
+       <div class="sp-c">邀请好友></div>
+      </div>
     </div>
-    <div class="pinglun">
-      <div class="top"></div>
-      <div class="bottom"></div>
     </div>
   </div>
+  <div class="xiaoxi">
+    <div class="xiaox-a">
+      <h3>消息</h3>
+      <grid>
+          <grid-item icon="photo-o" text="被赞" />
+          <grid-item icon="photo-o" text="被评论" />
+          <grid-item icon="photo-o" text="被收藏" />
+          <grid-item icon="photo-o" text="被关注" />
+        </grid>
+    </div>
+  </div>
+  <div class="dingdan">
+    <div class="dingd-a">
+      <h3>我的订单</h3>
+      <grid>
+          <grid-item icon="photo-o" text="被赞" />
+          <grid-item icon="photo-o" text="被评论" />
+          <grid-item icon="photo-o" text="被收藏" />
+          <grid-item icon="photo-o" text="被关注" />
+        </grid>
+    </div>
+  </div>
+  <div class="fuwu">
+    <div class="fuw-a">
+      <h3>更多服务</h3>
+      <div class="fuw-b">
+        <img src="./images/Slice 117.jpg" alt="">
+        <samp class="sa-a">阅览记录</samp>
+        <samp class="sa-b">></samp>
+      </div>
+      <div class="fuw-b">
+        <img src="./images/Slice 118.jpg" alt="">
+        <samp class="sa-a">阅览记录</samp>
+        <samp class="sa-b">></samp>
+      </div>
+      <div class="fuw-b">
+        <img src="./images/Slice 119.jpg" alt="">
+        <samp class="sa-a">阅览记录</samp>
+        <samp class="sa-b">></samp>
+      </div>
+      <div class="fuw-b">
+        <img src="./images/Slice 120.jpg" alt="">
+        <samp class="sa-a">阅览记录</samp>
+        <samp class="sa-b">></samp>
+      </div>
+      <div class="fuw-b">
+        <img src="./images/Slice 113.jpg" alt="">
+        <samp class="sa-a">阅览记录</samp>
+        <samp class="sa-b">></samp>
+      </div>
+      <div class="fuw-b">
+        <img src="./images/Slice 115.jpg" alt="">
+        <samp class="sa-a">阅览记录</samp>
+        <samp class="sa-b">></samp>
+      </div>
+      <div class="fuw-b" style="margin-bottom: 31px;">
+        <img src="./images/Slice 116.jpg" alt="">
+        <samp class="sa-a">阅览记录</samp>
+        <samp class="sa-b">></samp>
+      </div>
+    </div>
+  </div>
+  
 </template>
+<script setup>
+import { Grid, GridItem } from 'vant';
+</script>
